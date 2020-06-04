f8127c6cb9540784368ac8886dcaf562c9b2c787
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-06-04 10:56:47 +0530

    files uploaded
    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Gallery from './Component/Gallery';
+import './Component/Gallery.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Gallery/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Gallery.jsx
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import './Gallery.css';
+export default class Gallery extends Component {
+    render(){
+        return(
+            <div class="photos">
+                <h1>Image Gallery</h1>
+                
+                <img width="200px" height="200px"
+                src="../img/ratnagiri-1.jpg" alt="Ratnagiri"
+                />
+                 <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=" "
+                />
+                
+                <img width="200px" height="200px"
+                src="../img/sindhudurg.jpg" alt="Sindhudurg"
+                />
+                 <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=" "
+                />
+                
+                <img width="200px" height="200px"
+                src="../img/solapur.jpg" alt="Solapur"
+                />
+                <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=" "
+                />
+               
+                <img width="200px" height="200px" 
+                src="../img/kolhapur.jpg" alt="Kolhapur "
+                />
+                <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=""
+                />
+                
+                <img width="200px" height="200px"
+                src="../img/alibaug-2.jpg" alt="Alibaug"
+                />
+                 <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=" "
+                />
+                
+                <img width="200px" height="200px"
+                src="../img/aurangabad.jpg" alt="Aurangabad"
+                />
+                 <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=" "
+                />
+                
+                <img width="200px" height="200px" 
+                src="../img/mahabaleshwar-7.jpg" alt="Mahabaleshwar"
+                />
+                 <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=" "
+                />
+                
+                <img width="200px" height="200px"
+                src="../img/matheran-4.jpg" alt="Matheran"
+                />
+                
+                <img width="200px" height="200px" 
+                src="../img/space.jpg" alt=" "
+                />
+                
+                <img width="200px" height="200px"
+                src="../img/mumbai-3.jpg" alt="Mumbai"
+                />
+                
+               
+
+            </div>
+            
+            
+        )
+    }
+}
